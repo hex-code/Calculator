f8ac0fd0c7806cc8b32fc737550572370fd76da3
hex-code <GrozaRazor@gmail.com>
2014-06-19 18:40:24 +0300

    modified main.js

--- js/main.js
+++ js/main.js
@@ -8,13 +8,22 @@ var Calc = {
 		if (isNaN(this.numbPos.id) && this.numbPos.id != 'butC' && this.numbPos.id != '='){
 		op = this.numbPos.id;
 		opA = resArea.value;
-		resArea.value = '';
+		resArea.value = opA + op;
 		} else if (this.numbPos.id === 'butC') {
 			resArea.value = '';
 		} else if (this.numbPos.id === '=') {
 			var arr = resArea.value;
-			opB = resArea.value;
-			var src = opA + op + opB;
+			var b = parseInt(opA);
+			for (var i = 0; i < arr.length; i++) {
+				if ( arr.charAt(i) === op ) {
+						var cache = i + 1;
+						opB = '';
+					for (var j = arr.length - (++i); j > 0; j--) {
+						opB += arr.charAt(cache);
+						cache++;
+					}
+				}
+			}
 			if ( op === '/') {
 				var res = parseInt(opA) / parseInt(opB);
 			} else if ( op === '*' ) {
@@ -24,7 +33,7 @@ var Calc = {
 			} else {
 				var res = parseInt(opA) - parseInt(opB);
 			}
-			resArea.value = src + '=' + res;
+			resArea.value = res;
 		}
 	},
 	number: function() {
